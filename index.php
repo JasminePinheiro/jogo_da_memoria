6a1590650b06bfa1d74c3739d5604131313c1d2f
Jasmine Pinheiro <jasminepinheiro14@gmail.com>
2022-09-15 10:20:07 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jogo da memória</title>
+    <link rel="shortcut" href="imagens/game.ico" type="image/x-ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@700&family=Sriracha&display=swap" rel="stylesheet">
+    <link rel="stylesheet"  href="css/reset.css">
+    <link rel="stylesheet"  href="css/body.css">
+    <link rel="stylesheet"  href="css/header.css">
+    <link rel="stylesheet"  href="css/main.css">
+    <link rel="stylesheet"  href="css/footer.css">
+</head>
+<body>
+
+    <header>
+    <a href="https://www.instagram.com/kj_games1/" target="blank">
+    <img src="imagens/logo.png" alt="logo"></a>
+        <nav>
+             <ul class="links-container">
+                <li>
+                    <a href="#">HOME</a>
+                </li>
+                 <li>
+                    <a href="#">COMO JOGAR?</a>
+                </li>
+                <li>
+                    <a href="#">CURIOSIDADES</a>
+                </li>
+
+            <ul>
+        </nav>
+    </header>
+   <main>
+    <div class="content-container">
+    <div class="caixa"><h1 id="titulo">JOGO DA <br>
+    MEMÓRIA</h1></div>
+    </div>
+    <div class="social-container"></div>
+    
+    </main>
+   
+   <footer>
+    
+      
+   </footer>
+</body>
+</html>
