1228c1a3929657d22a0ab0a506f7d9c55974ad00
Jasmine Pinheiro <jasminepinheiro14@gmail.com>
2022-10-16 23:09:43 -0300

    mudanças

---
+++ niveis.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/niveis.css">
+    <title>niveis</title>
+</head>
+<body>
+<div class="header">
+        <div class="logo">
+        <a href="https://www.instagram.com/kj_games1/" target="blank">
+                <img src="imagens/logo1.png" alt="logo">
+            </a>
+        </div>
+    </div>
+
+    <div class="niveis_body">
+            <h2>Níveis</h2>
+            <form action="#" method="#">
+                <div class="nivel_facil">
+                    <button>
+                    <a href="game_facil.php">FÁCIL</a>
+                    </button>
+                </div><br>
+
+                <div class="nivel_medio">
+                    <a href="game_media.php"><button class="submit" name="btnmedio" value="dificil"></a>
+                        MÉDIO
+                    </button>
+                </div><br>
+                
+
+                <div class="nivel_dificil">
+                    <a href="game_dificil.php"><button class="submit" name="btndificil" value="dificil"></a>
+                        DIFÍCIL
+                    </button>
+                </div>
+            </form>
+
+            <div class="nivel_footer">
+                
+                <div class="voltar">
+                <a href="index.php"><p>Voltar para Página Inicial</a></p>
+                </div>
+
+               
+            </div>
+        </div>
+    </div>
+</body>
+</html>
